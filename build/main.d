271da263022253bbb5452daad62fba532f983d99
Cooper Keely <ckeely@uw.edu>
2025-07-21 19:38:14 -0700

    Removed build directory from git repo

--- build/main.d
+++
@@ -1,3 +0,0 @@
-build/main.o: src/main.c src/../include/log.h src/../include/arc_mutex.h
-src/../include/log.h:
-src/../include/arc_mutex.h:
--- build/queue.d
+++
@@ -1,3 +0,0 @@
-build/queue.o: src/queue.c src/../include/log.h src/../include/queue.h
-src/../include/log.h:
-src/../include/queue.h:
